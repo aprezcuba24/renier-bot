96e0ead9e8bed1fe8a4c9daa54d37854d01e204a
Renier Ricardo Figueredo <aprezcuba24@gmail.com>
2017-01-13 14:06:22 -0500

    test

--- src/server.js
+++ src/server.js
@@ -3,16 +3,24 @@ var PORT=process.env.PORT;
 function handleRequest(request, response){
   response.end(
     '<html>' +
+    '<meta charset="UTF-8">' +
     '<body>' +
     'Responde a una conversación de este tipo' +
     '<br/>' +
     'humano: plan' +
+    '<br/>' +
     'bot: ¿Qué hiciste ayer?' +
+    '<br/>' +
     'humano: code 1' +
+    '<br/>' +
     'bot: ¿Algo más?' +
+    '<br/>' +
     'humano: code 2' +
+    '<br/>' +
     'bot: ¿Algo más?' +
+    '<br/>' +
     'humano: más nada' +
+    '<br/>' +
     'OK! nos vemos' +
     '<br/>' +
     '</body>' +
